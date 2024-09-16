8d1e97ce4b6cb2006fd1ed405aecf69e58cb4752
Mouayad <muayad.perun@outlook.com>
2024-09-16 18:19:08 +0300

    refactor: move logger to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { env } from "@/common/utils/envConfig";
-import { app, logger } from "@/server";
+import { app } from "@/server";
+import { env } from "@utils/envConfig";
+import { logger } from "@utils/logger";
 
 const server = app.listen(env.PORT, () => {
   const { NODE_ENV, HOST, PORT } = env;
