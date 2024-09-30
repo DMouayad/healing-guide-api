628eccb038e7efba76dd6aa9b02fdcc686795933
Mouayad <muayad.perun@outlook.com>
2024-09-30 09:11:16 +0300

    feat: use logger `short` format for dev env

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,7 +1,7 @@
 import { env } from "@/common/utils/envConfig";
 import morgan from "morgan";
-export const requestLogger = morgan(env.isDevelopment ? "dev" : "combined", {
-	skip(req, res) {
+export const requestLogger = morgan(env.isDevelopment ? "short" : "combined", {
+	skip(_req, res) {
 		return res.statusCode < 400;
 	},
 });
