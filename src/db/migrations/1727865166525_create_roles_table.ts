483f28e8e4c3f5a513173b779e7a3691df4504d7
Mouayad <muayad.perun@outlook.com>
2024-10-02 16:20:03 +0300

    feat: add roles table db migration

---
+++ src/db/migrations/1727865166525_create_roles_table.ts
@@ -0,0 +1,15 @@
+import { type Kysely, sql } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.createTable("roles")
+		.addColumn("id", "bigserial", (col) => col.primaryKey())
+		.addColumn("slug", "varchar(255)", (col) => col.notNull())
+		.addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
+		.execute();
+	await db.schema.createIndex("roles_slug_index").on("roles").column("slug").execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.dropTable("roles").ifExists().execute();
+}
