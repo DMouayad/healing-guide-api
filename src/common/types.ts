c2ff41895587bf1b3300e56d3da25c1706a32e0c
Mouayad <muayad.perun@outlook.com>
2024-10-02 14:57:50 +0300

    feat: add `Role` type & define roles list

--- src/common/types.ts
+++ src/common/types.ts
@@ -20,3 +20,14 @@ export type AuthState = {
 	personalAccessToken: AccessToken | undefined;
 	user: IUser | undefined;
 };
+
+type ObjectValues<T> = T[keyof T];
+export type Role = ObjectValues<typeof APP_ROLES>;
+
+export const APP_ROLES = {
+	guest: { slug: "guest" },
+	admin: { slug: "admin" },
+	patient: { slug: "patient" },
+	facilityManager: { slug: "facilityManager" },
+	physician: { slug: "physician" },
+} as const;
