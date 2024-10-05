a2d272344a7537ce22d19496c8814e3aa1cf3c9b
Mouayad <muayad.perun@outlook.com>
2024-10-05 17:21:30 +0300

    fix: don't transform id in zod schemas

--- src/common/utils/zodSchemas.ts
+++ src/common/utils/zodSchemas.ts
@@ -5,7 +5,6 @@ export const commonZodSchemas = {
 		.string()
 		.refine((data) => !Number.isNaN(Number(data)), "ID must be a numeric value")
 		.transform(Number)
-		.refine((num) => num > 0, "ID must be a positive number")
-		.transform((num) => num.toString()),
+		.refine((num) => num > 0, "ID must be a positive number"),
 	// ... other common validations
 };
