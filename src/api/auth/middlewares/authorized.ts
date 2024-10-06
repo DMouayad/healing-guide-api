e4aaffd05f5dc68bd7f26bb4e696d4b54bea6fab
Mouayad <muayad.perun@outlook.com>
2024-10-06 14:26:51 +0300

    feat: add `authorized` middleware

---
+++ src/api/auth/middlewares/authorized.ts
@@ -0,0 +1,29 @@
+import AppError from "@/common/models/appError";
+import type { Role } from "@/common/types";
+import type { IUser } from "@/interfaces/IUser";
+import type { NextFunction, Request, Response } from "express";
+
+export function authorized(role: Role) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const user: IUser | undefined = res.locals.auth?.user;
+		if (!user) {
+			throw AppError.UNAUTHORIZED();
+		}
+		if (user.isAuthorizedAs(role)) {
+			next();
+		}
+		throw AppError.FORBIDDEN();
+	};
+}
+export function authorizedAsAny(roles: Role[]) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const user: IUser | undefined = res.locals.auth?.user;
+		if (!user) {
+			throw AppError.UNAUTHORIZED();
+		}
+		if (user.isAuthorizedAsAny(roles)) {
+			next();
+		}
+		throw AppError.FORBIDDEN();
+	};
+}
