40d7b4c36c24552c72d5204c3a592b5712f4908f
Mouayad <muayad.perun@outlook.com>
2024-10-07 14:49:24 +0300

    feat: create separate "success" and "failure" subclasses of `ActionResult`

--- src/common/models/actionResult.ts
+++ src/common/models/actionResult.ts
@@ -1,32 +1,62 @@
+import type { IProvidesApiResponse } from "@/interfaces/IProvidesApiResponse";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
+import type { ApiResponse, ClassProperties } from "../types";
+import type AppError from "./appError";
 
-export class ActionResult<T = null> {
-	readonly success: boolean;
-	readonly message: string;
-	readonly responseObject: T;
-	readonly statusCode: number;
+export type ActionResultProps = ClassProperties<ActionResult>;
+export abstract class ActionResult implements IProvidesApiResponse {
+	public readonly statusCode: number;
 
-	private constructor(success: boolean, message: string, responseObject: T, statusCode: number) {
-		this.success = success;
-		this.message = message;
-		this.responseObject = responseObject;
-		this.statusCode = statusCode;
+	constructor(props: ActionResultProps) {
+		this.statusCode = props.statusCode;
+	}
+	toApiResponse(): ApiResponse {
+		let response: ApiResponse;
+		if (this instanceof SuccessActionResult) {
+			response = { data: this.responseObject };
+		} else if (this instanceof FailureActionResult) {
+			response = { appError: this.error };
+		}
+		response ??= {};
+		return response;
 	}
 
-	static success<T>(message: string, responseObject?: T, statusCode: number = StatusCodes.OK) {
-		return new ActionResult(true, message, responseObject, statusCode);
+	static success(props?: Partial<ClassProperties<SuccessActionResult>>) {
+		return new SuccessActionResult({
+			statusCode:
+				(props?.statusCode ?? props?.responseObject === undefined)
+					? StatusCodes.NO_CONTENT
+					: StatusCodes.OK,
+			responseObject: props?.responseObject,
+		});
 	}
 
-	static failure<T>(message: string, responseObject: T, statusCode: number = StatusCodes.BAD_REQUEST) {
-		return new ActionResult(false, message, responseObject, statusCode);
+	static failure(props: Partial<ActionResultProps> & { error: AppError }): ActionResult {
+		return new FailureActionResult({
+			statusCode: props?.statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR,
+			error: props.error,
+		});
+	}
+}
+
+class SuccessActionResult extends ActionResult {
+	public readonly responseObject?: object;
+	constructor(props: ActionResultProps & { responseObject?: object }) {
+		super(props);
+		this.responseObject = props.responseObject;
+	}
+}
+class FailureActionResult extends ActionResult {
+	public readonly error: AppError;
+	constructor(props: ActionResultProps & { error: AppError }) {
+		super(props);
+		this.error = props.error;
 	}
 }
 
 export const ActionResultSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
 	z.object({
-		success: z.boolean(),
-		message: z.string(),
 		responseObject: dataSchema.optional(),
 		statusCode: z.number(),
 	});
