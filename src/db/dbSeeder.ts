1d0ab94eabdc41c617ef41f5ac7b0472d89a0399
Mouayad <muayad.perun@outlook.com>
2024-10-11 07:43:09 +0300

    feat: add `seedAdmin` helper

--- src/db/dbSeeder.ts
+++ src/db/dbSeeder.ts
@@ -1,4 +1,5 @@
 import { seed as rolesSeed } from "./seeds/1728017019540_seed_roles";
+import { seed as adminUserSeed } from "./seeds/1728018124296_seed_admin";
 
 import { prepareUserToInsertWithKysely } from "@/api/user/user.model";
 import { userFactory } from "@/common/factories/userFactory";
@@ -6,6 +7,13 @@ import { APP_ROLES, type Role } from "@/common/types";
 import { logger } from "@/common/utils/logger";
 import { db } from ".";
 
+export async function seedAdmin(): Promise<void> {
+	return adminUserSeed(db)
+		.then((_) => logger.info("SEED SUCCESS(admin-user)"))
+		.catch((error) => {
+			logger.error(`SEED FAILED(admin-user): ${error}`);
+		});
+}
 export async function seedAppRoles(): Promise<void> {
 	return rolesSeed(db)
 		.then((_) => logger.info("SEED SUCCESS(roles)"))
