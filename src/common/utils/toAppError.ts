d90591f4385628048a00181325c89dbfa8e429c3
Mouayad <muayad.perun@outlook.com>
2024-10-20 21:25:14 +0300

    fix: keep original `Error` message
    - simplify code

--- src/common/utils/toAppError.ts
+++ src/common/utils/toAppError.ts
@@ -3,30 +3,26 @@ import AppError from "../models/appError";
 import { APP_ERR_CODES } from "../models/errorCodes";
 
 export function toAppError(err: any): AppError {
-	if (isZodError(err)) {
+	if (err instanceof ZodError) {
 		return AppError.VALIDATION({ message: JSON.stringify(err.format()) });
 	}
-	if (isAppErr(err)) {
+	if (err instanceof AppError) {
 		err.stack = refineStackTrace(err.stack);
 		return err;
 	}
-	const errStatusCode = err.status || err.statusCode || 500;
-	const errMessage = errStatusCode === 500 ? "Internal Server Error" : err.message;
-	return new AppError(
-		errMessage,
-		errStatusCode,
-		errStatusCode === 500 ? APP_ERR_CODES.SERVER : APP_ERR_CODES.UNKNOWN,
-	);
+	return getAppErrorFrom(err);
+}
+function getAppErrorFrom(err: any): AppError {
+	const statusCode = err?.status || err?.statusCode || 500;
+	const message = err?.message ?? "Internal Server Error";
+	const errCode = statusCode === 500 ? APP_ERR_CODES.SERVER : APP_ERR_CODES.UNKNOWN;
+	const appError = new AppError(message, statusCode, errCode);
+	appError.stack = err?.stack;
+	return appError;
 }
 function refineStackTrace(trace: string | undefined) {
 	if (trace) {
 		const stackTraceLines: string[] | undefined = trace.split("\n");
 		return stackTraceLines?.slice(3).join("\n");
 	}
-}
-function isAppErr(err: any): err is AppError {
-	return err instanceof AppError || (err as AppError).errCode !== undefined;
-}
-function isZodError(err: any) {
-	return err instanceof ZodError;
 }
