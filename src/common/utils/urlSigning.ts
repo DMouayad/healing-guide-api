8adf4f9eec64397a0b19e3b934945cd07f919711
Mouayad <muayad.perun@outlook.com>
2024-11-19 10:56:50 +0200

    refactor: add `userRoutes`

--- src/common/utils/urlSigning.ts
+++ src/common/utils/urlSigning.ts
@@ -1,9 +1,10 @@
+import { userRoutes } from "@/api/user/user.router";
 import type { IUser } from "@/interfaces/IUser";
 import signed from "signed";
 import { env } from "./envConfig";
 
 export function createVerifyEmailURL(user: IUser) {
-	const url = `${env.APP_URL}/api/${env.API_VERSION}/users/email/verify/${user.id}`;
+	const url = `${env.APP_URL}/api/${env.API_VERSION}/users${userRoutes.verifyEmail(user.id.toString())}`;
 	return getUserSignature(user).sign(url, { method: "get" });
 }
 
