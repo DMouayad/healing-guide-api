112fb66941089d854de601a2891ed024d1c3ed45
Mouayad <muayad.perun@outlook.com>
2024-12-22 18:19:56 +0200

    chore: rm unused code

--- src/db/kyselyRelations.ts
+++
@@ -1,9 +0,0 @@
-import type { Expression } from "kysely";
-import { jsonObjectFrom } from "kysely/helpers/postgres";
-import { db } from ".";
-
-export function addRoleToUser(roleId: Expression<number>) {
-	return jsonObjectFrom(
-		db.selectFrom("roles").select(["id as roleId", "slug"]).where("id", "=", roleId),
-	).$notNull();
-}
