a102e26bc757c1213733c0fbc2147c4970353d1d
Mouayad <muayad.perun@outlook.com>
2025-01-03 16:58:33 +0200

    feat: handle rate limit errors in app `errorHandler`

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,10 @@
 import ApiResponse from "@/common/models/apiResponse";
+import { getRetryAfterSecs } from "@/common/rateLimiters";
 import { toAppError } from "@/common/utils/toAppError";
 import { logErrIfNeeded, logger } from "@utils/logger";
 import type { ErrorRequestHandler, RequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
+import { RateLimiterRes } from "rate-limiter-flexible";
 
 export const unexpectedRequestHandler: RequestHandler = (req, res) => {
 	logger.warn(`${req.method} Request made to unknown route: ${req.url}`);
@@ -10,8 +12,12 @@ export const unexpectedRequestHandler: RequestHandler = (req, res) => {
 };
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-	const appError = toAppError(err);
-	logErrIfNeeded(appError);
-	// send a json response containing the error
-	ApiResponse.error(appError).send(res);
+	if (err instanceof RateLimiterRes) {
+		ApiResponse.rateLimited({ retryAfterSecs: getRetryAfterSecs(err) }).send(res);
+	} else {
+		const appError = toAppError(err);
+		logErrIfNeeded(appError);
+		// send a json response containing the error
+		ApiResponse.error(appError).send(res);
+	}
 };
