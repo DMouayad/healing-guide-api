6375a23d535ef591649c31fb683ef976c929c995
Mouayad <muayad.perun@outlook.com>
2025-01-04 14:30:11 +0200

    fix(rateLimiter): add missing function call

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -18,7 +18,7 @@ function baseRateLimiter(limiter: RateLimiterAbstract, key?: string | number) {
 				.catch(async (err) => {
 					if (err instanceof RateLimiterRes) {
 						const retryAfterSecs = getRetryAfterSecs(err);
-						return Promise.reject(AppError.RATE_LIMIT_EXCEEDED({ retryAfterSecs }));
+						throw AppError.RATE_LIMIT_EXCEEDED({ retryAfterSecs });
 					}
 					return Promise.reject(err);
 				});
@@ -28,20 +28,20 @@ function baseRateLimiter(limiter: RateLimiterAbstract, key?: string | number) {
 
 export function rateLimiterByIP(limiter: RateLimiterAbstract) {
 	return (req: Request, res: Response, next: NextFunction) => {
-		return baseRateLimiter(limiter, getClientIp(req));
+		return baseRateLimiter(limiter, getClientIp(req))(req, res, next);
 	};
 }
 export function rateLimiterByUser(limiter: RateLimiterAbstract) {
 	return (req: Request, res: Response, next: NextFunction) => {
 		const user = getUserFromResponse(res);
-		return baseRateLimiter(limiter, user.id);
+		return baseRateLimiter(limiter, user.id)(req, res, next);
 	};
 }
 export function rateLimiterByEmailAndPhone(limiter: RateLimiterAbstract) {
 	return (req: Request, res: Response, next: NextFunction) => {
 		const creds = CredentialsForRateLimitSchema.parse(req.body);
 		const key = getCredsRateLimitingKey(creds);
-		return baseRateLimiter(limiter, key);
+		return baseRateLimiter(limiter, key)(req, res, next);
 	};
 }
 
