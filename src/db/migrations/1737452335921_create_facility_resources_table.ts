be3265ed10911a2254cd81e9e7c2f651be00e2a3
Mouayad <muayad.perun@outlook.com>
2025-01-21 11:50:35 +0200

    feat(#42): create `facility_resources` migration

---
+++ src/db/migrations/1737452335921_create_facility_resources_table.ts
@@ -0,0 +1,28 @@
+import type { Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.createTable("facility_resources")
+		.addColumn("facility_id", "int4", (cb) => cb.notNull())
+		.addColumn("category_id", "int4", (cb) => cb.notNull())
+		.addColumn("title", "varchar(255)", (cb) => cb.notNull())
+		.addColumn("content", "text", (cb) => cb.notNull())
+		.addForeignKeyConstraint(
+			"facility_resources_medical_facilities_FK",
+			["facility_id"],
+			"medical_facilities",
+			["id"],
+		)
+		.addForeignKeyConstraint(
+			"facility_resources_facility_resource_categories_FK",
+			["category_id"],
+			"facility_resource_categories",
+			["id"],
+		)
+		.ifNotExists()
+		.execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.dropTable("facility_resources").ifExists().execute();
+}
