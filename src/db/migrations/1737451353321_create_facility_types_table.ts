d6b32173640312c43e16ddb3719f6c3bbf09b1b9
Mouayad <muayad.perun@outlook.com>
2025-01-21 11:19:14 +0200

    feat(#42): create `medical_facilities` migration
    - feat: add `facility_types` table migration

---
+++ src/db/migrations/1737451353321_create_facility_types_table.ts
@@ -0,0 +1,14 @@
+import type { Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.createTable("facility_types")
+		.addColumn("id", "serial", (col) => col.primaryKey())
+		.addColumn("name", "varchar", (col) => col.notNull())
+		.ifNotExists()
+		.execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.dropTable("facility_types").ifExists().execute();
+}
